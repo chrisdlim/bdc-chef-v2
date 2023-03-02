54a71b9ec3ecabc19440d16b4071e0382b7421cf
christophlim <22702896+chrisdlim@users.noreply.github.com>
2023-03-02 12:38:05 -0500

    Sample test command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,9 @@
 import { ChatInputCommandInteraction, Client } from "discord.js";
 import { Ping } from "./ping";
+import { Test } from "./test";
 import { Command } from "./types";
 
-export const commands: Command[] = [Ping];
+export const commands: Command[] = [Ping, Test];
 
 export const registerApplicationCommands = (client: Client<true>) => {
   return client.application.commands.set(commands);
