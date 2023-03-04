8ddeeed7451f25d95d88cd9f39f3f7b6749c22c9
christophlim <22702896+chrisdlim@users.noreply.github.com>
2023-03-04 01:14:36 -0500

    Fixed ltg

--- src/commands/ltg.ts
+++ src/commands/ltg.ts
@@ -6,7 +6,8 @@ export const Ltg: Command = {
 	description: 'Lunch Time Gamers',
 	type: ApplicationCommandType.ChatInput,
 	run: async (_client: Client, interaction: ChatInputCommandInteraction) => {
+		const roleId = '716085401701318666';
 
-		await interaction.reply(`<@${interaction.user.id}> looking for some gamers!!! @Tilted Gamers`);
+		await interaction.reply(`<@${interaction.user.id}> looking for some gamers!!! <@&${roleId}>`);
 	}
 };
