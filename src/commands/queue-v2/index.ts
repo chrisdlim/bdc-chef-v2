d41d7530382a395584b0a86f5a6163c23adffb6b
christophlim <22702896+chrisdlim@users.noreply.github.com>
2023-05-01 10:18:36 -0400

    updated msg

--- src/commands/queue-v2/index.ts
+++ src/commands/queue-v2/index.ts
@@ -90,7 +90,7 @@ export const QueueV2: Command = {
 
     // Follow up with ping
     await interaction.followUp({
-      content: `${roleMention(config.tiltedGamersRoleId)} join if ur a queue-ty pie`,
+      content: `${roleMention(config.tiltedGamersRoleId)} join if ur a queuety pie`,
       allowedMentions: {
         parse: ['roles']
       }
